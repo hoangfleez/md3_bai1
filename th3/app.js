ff3f8f54905a33ce63f0af61907f20f48f60355a
hoang.le <leehoang@gmail.com>
2023-01-09 11:36:17 +0700

    debug

--- th3/app.js
+++ th3/app.js
@@ -1,7 +1,7 @@
 const checkLicensePlateInHanoi = (licensePlate) => {
     const array = ['29', '30', '31', '32', '33', '40'];
     for (const value of array) {
-        if (licensePlate.includes(value)) {
+        if (licensePlate.startsWith(value)) {
             return true;
         }
     }
